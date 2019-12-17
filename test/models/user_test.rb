93bea7380343a7d4e33547dbad4a3e7253c41797
塩沢　光貴 <ko.shiozawa@a1706-036.opt-sys.local>
2019-12-17 14:37:05 +0900

    12月17

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -22,4 +22,20 @@ class UserTest < ActiveSupport::TestCase
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
